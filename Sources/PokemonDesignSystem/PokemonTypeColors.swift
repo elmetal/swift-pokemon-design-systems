9f0da7cfb52cb6937da1dfb43f3e4c47be7abff5
elmetal <nm7.pupu@gmail.com>
2026-05-16 14:03:50 +0900

    add color

---
+++ Sources/PokemonDesignSystem/PokemonTypeColors.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+public extension Color {
+    static let pokemonNormalTint = Color(red: 168 / 255, green: 167 / 255, blue: 122 / 255)
+    static let pokemonFireTint = Color(red: 238 / 255, green: 129 / 255, blue: 48 / 255)
+    static let pokemonWaterTint = Color(red: 99 / 255, green: 144 / 255, blue: 240 / 255)
+    static let pokemonElectricTint = Color(red: 247 / 255, green: 208 / 255, blue: 44 / 255)
+    static let pokemonGrassTint = Color(red: 122 / 255, green: 199 / 255, blue: 76 / 255)
+    static let pokemonIceTint = Color(red: 150 / 255, green: 217 / 255, blue: 214 / 255)
+    static let pokemonFightingTint = Color(red: 194 / 255, green: 46 / 255, blue: 40 / 255)
+    static let pokemonPoisonTint = Color(red: 163 / 255, green: 62 / 255, blue: 161 / 255)
+    static let pokemonGroundTint = Color(red: 226 / 255, green: 191 / 255, blue: 101 / 255)
+    static let pokemonFlyingTint = Color(red: 169 / 255, green: 143 / 255, blue: 243 / 255)
+    static let pokemonPsychicTint = Color(red: 249 / 255, green: 85 / 255, blue: 135 / 255)
+    static let pokemonBugTint = Color(red: 166 / 255, green: 185 / 255, blue: 26 / 255)
+    static let pokemonRockTint = Color(red: 182 / 255, green: 161 / 255, blue: 54 / 255)
+    static let pokemonGhostTint = Color(red: 115 / 255, green: 87 / 255, blue: 151 / 255)
+    static let pokemonDragonTint = Color(red: 111 / 255, green: 53 / 255, blue: 252 / 255)
+    static let pokemonDarkTint = Color(red: 112 / 255, green: 87 / 255, blue: 70 / 255)
+    static let pokemonSteelTint = Color(red: 183 / 255, green: 183 / 255, blue: 206 / 255)
+    static let pokemonFairyTint = Color(red: 214 / 255, green: 133 / 255, blue: 173 / 255)
+}
